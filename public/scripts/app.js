fc17a8d31d5567afd7d28949127beffcab1b7523
Egidio <Rafael.Egidio@unisys.com>
2017-10-27 18:19:47 -0200

    utilizando object

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,36 +2,70 @@
 
 console.log('App.js is runing!');
 
-// JSX - JAvascript XML
+var app = {
+    title: 'Indecision  app',
+    subtile: ' this is from object'
+};
 
 var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    'Indecisioning App'
-  ),
-  React.createElement(
-    'p',
-    null,
-    'This is some info'
-  ),
-  React.createElement(
-    'o1',
+    'div',
     null,
     React.createElement(
-      'li',
-      null,
-      'Item 1'
+        'h1',
+        null,
+        app.title
     ),
     React.createElement(
-      'li',
-      null,
-      'Item 2'
+        'p',
+        null,
+        app.subtile
+    ),
+    React.createElement(
+        'o1',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item 1'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item 2'
+        )
     )
-  )
 );
-var appRoot = document.getElementById('app');
 
+var user = {
+    name: 'Rafael',
+    age: 31,
+    location: 'Nova Lima'
+
+};
+
+var templateDois = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name + '!'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age:',
+        user.age,
+        ' '
+    ),
+    React.createElement(
+        'p',
+        null,
+        ' location: ',
+        user.location,
+        ' '
+    )
+);
+
+var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,18 +1,37 @@
 console.log('App.js is runing!')
 
 
-// JSX - JAvascript XML
+var app ={
+    title: 'Indecision  app',
+    subtile:' this is from object'
+}
 
 var template = (
 <div>
- <h1>Indecisioning App</h1>
- <p>This is some info</p>
+ <h1>{app.title}</h1>
+ <p>{app.subtile}</p>
  <o1>
     <li>Item 1</li>
     <li>Item 2</li>
  </o1>
 </div>
 );
-var appRoot = document.getElementById('app');
 
+
+var user ={
+    name :'Rafael',
+    age: 31,
+    location : 'Nova Lima'
+
+}
+
+var templateDois = (
+    <div>
+        <h1>{user.name + '!'}</h1>
+        <p>Age:{user.age} </p>
+        <p> location: {user.location} </p>
+    </div>
+)
+
+var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot );
