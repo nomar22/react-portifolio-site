cd7aca12da470c37e914bbb3e51b258ac47f6f81
Egidio <nomar22@gmail.com>
2017-11-04 20:35:43 -0200

    data Binding + form submition

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,21 +1,85 @@
 'use strict';
 
-var add = function add(a, b) {
-    //console.log(arguments);
-    return a + b;
+var app = {
+    title: 'Indecision  App',
+    subtile: ' this is from object',
+    options: []
 };
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
 
-console.log(add(2, 3, 20));
+    var option = e.target.elements.opcao.value;
 
-var user = {
-    name: 'Rafael',
-    cities: ['bh', 'contagem', 'brisbane'],
-    printPlacesLived: function printPlacesLived() {
-        console.log(this.name);
-        console.log(this.cities);
-
-        this.cities.forEach;
+    if (option) {
+        app.options.push(option);
+        e.target.elements.opcao.value = '';
     }
+
+    renderApp();
 };
 
-user.printPlacesLived();
+var removeOptions = function removeOptions() {
+    app.options = [];
+    renderApp();
+};
+
+var appRoot = document.getElementById('app');
+
+var renderApp = function renderApp() {
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            app.title
+        ),
+        app.subtile && React.createElement(
+            'p',
+            null,
+            app.subtile
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length > 0 ? 'Here your  Options' : 'No Options'
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length
+        ),
+        React.createElement(
+            'o1',
+            null,
+            React.createElement(
+                'li',
+                null,
+                'Item 1'
+            ),
+            React.createElement(
+                'li',
+                null,
+                'Item 2'
+            )
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'opcao' }),
+            React.createElement(
+                'button',
+                null,
+                ' Add Option '
+            ),
+            React.createElement(
+                'button',
+                { onClick: removeOptions },
+                ' Remove All '
+            )
+        )
+    );
+    ReactDOM.render(template, appRoot);
+};
+
+renderApp();
