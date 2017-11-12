ce3d99a1ff7498ede86741f5bfc95ad67bb70ea4
Egidio <nomar22@gmail.com>
2017-11-12 21:51:25 -0200

    basic webpack configured

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,13 +1,13 @@
-//count - setup default prop value to 0
+
 class Counter extends React.Component {
     constructor(props) {
         super(props);
         this.handleAddOne = this.handleAddOne.bind(this);
         this.handleMinusOne = this.handleMinusOne.bind(this);
         this.handleReset = this.handleReset.bind(this);
-    
+
         this.state = {
-            count: props.count
+            count: 0
         };
     }
 
@@ -20,21 +20,49 @@ class Counter extends React.Component {
 
     }
 
+
+    componentDidMount() {
+        try {
+            const stringCount = localStorage.getItem('count');
+            const count = parseInt(stringCount);
+
+            if (!isNaN(count)) {
+                this.setState(() => ({ count }));
+            }
+
+        } catch (e) {
+
+        }
+
+        console.log('mount');
+    }
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.count != prevState.count) {
+            const json = JSON.stringify(this.state.count);
+            localStorage.setItem('count', json);
+            console.log('saving');
+
+        }
+    }
+    componentWillUnmount() {
+        console.log('Unmount');
+    }
+
     handleMinusOne() {
         this.setState((previousState) => {
             return {
-                count: previousState.count -1
+                count: previousState.count - 1
             };
         });
     }
 
     handleReset() {
-        this.setState(()=>{
+        this.setState(() => {
             return {
                 count: 0
             }
         });
-       
+
     }
 
     render() {
@@ -49,10 +77,8 @@ class Counter extends React.Component {
     }
 }
 
-Counter.defaultProps ={
-    count : 0
-}
 
 
-ReactDOM.render(<Counter  />, document.getElementById('app'));
+
+ReactDOM.render(<Counter />, document.getElementById('app'));
 
