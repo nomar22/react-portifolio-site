44b252abdf8fa646f584f7e44bf7340036bb20ba
Egidio <nomar22@gmail.com>
2017-11-13 23:37:32 -0200

    webpack runing

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-console.log('app.js is runiiaaang!');
+//  install-import and use
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const template =<p>This is JSX from WEBPACK</p>;
+
+
+ReactDOM.render(template, document.getElementById('app'));
---
+++ src/person.js
@@ -0,0 +1,8 @@
+console.log('person');
+
+export const isAdult = (age)=> age>=18;
+
+export const canDrink = (age)=> age>=18;
+
+
+export default (age)=> age>=65;
