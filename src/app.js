0c2c3bd04d3d09ed77c070e34f479c3948be4edc
Egidio <nomar22@gmail.com>
2017-11-30 13:16:31 -0200

    expensify-app with route

--- src/app.js
+++ src/app.js
@@ -1,7 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<IndecisionApp/>,document.getElementById('app'));
+
+const ExpenseDashBoardPage = () => (
+    <div>
+        This is my dashBoard Component
+    </div>
+);
+
+const AddExpensePage = () => (
+    <div>
+        This is my AddExpense Component
+    </div>
+);
+
+//edit 
+const EditExpensePage = () =>(
+    <div>This is my edit expense page</div>
+);
+
+//help
+const HelpPage = ()=>(
+    <div> This is my help page </div>
+);
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={ExpenseDashBoardPage} exact={true}/>
+            <Route path="/create" component={AddExpensePage}/>
+            <Route path="/edit" component={EditExpensePage}/>
+            <Route path="/help" component={HelpPage}/>
+        </div>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
