576498af14f5c7d13cd654eb33a24327452458a1
Egidio <nomar22@gmail.com>
2017-12-11 19:25:53 -0200

    router dynamic

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 //edit 
-const EditExpensePage = () => (
-    <div>This is my edit expense page</div>
-);
+const EditExpensePage = (props) => {
+    console.log(props);
+    return (
+        <div>
+            Editing the expense with id of {props.match.params.id}
+    </div>
+    );
+
+};
 export default EditExpensePage;
--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,6 @@ const Header = () => (
         <h1>Expensify</h1>
         <NavLink to="/" activeClassName="is-active" exact={true} > DashBoard /</NavLink>
         <NavLink to="/create" activeClassName="is-active" > Criar /</NavLink>
-        <NavLink to="/edit" activeClassName="is-active"> Editar/</NavLink>
         <NavLink to="/help" activeClassName="is-active"> Ajuda</NavLink>
     </header>
 );
